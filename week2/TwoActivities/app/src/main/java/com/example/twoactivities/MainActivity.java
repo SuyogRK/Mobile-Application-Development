cc15a102906fa12d72752650bc2724fa45f13a6a
SuyogRK <suyong59@gmail.com>
2019-02-19 16:56:37 +0545

    "Hello Toast Challenge git error changed"

--- week2/TwoActivities/app/src/main/java/com/example/twoactivities/MainActivity.java
+++ week2/TwoActivities/app/src/main/java/com/example/twoactivities/MainActivity.java
@@ -1,13 +1,26 @@
 package com.example.twoactivities;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+
+    private EditText changeText;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void startNewActivity(View view) {
+
+        Intent intent = new Intent(this, Main2Activity.class);
+        startActivity(intent);
+
     }
 }
